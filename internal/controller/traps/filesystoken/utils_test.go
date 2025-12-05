a57ff2a8a98f28b3442d48cf807e4dbfddc7729f
San7o <santigio2003@gmail.com>
2025-12-05 17:32:38 +0100

    fix(kive): go mod tidy, regenerated code
    Signed-off-by: Giovanni Santini <giovanni.santini@proton.me>

--- internal/controller/traps/filesystoken/utils_test.go
+++ internal/controller/traps/filesystoken/utils_test.go
@@ -93,7 +93,7 @@ var _ = Describe("generateTetragonTracingPolicy", func() {
 
 				// Check the container selector
 				for _, resourceFilter := range trap.MatchResources.Any {
-					if matching.ContainerSelectorSelectsAll(resourceFilter.ResourceDescription.ContainerSelector) {
+					if matching.ContainerSelectorSelectsAll(resourceFilter.ContainerSelector) {
 						Expect(tracingPolicy.Spec.ContainerSelector.MatchExpressions).To(BeEmpty())
 					} else {
 						Expect(tracingPolicy.Spec.ContainerSelector.MatchExpressions).To(HaveLen(1))
