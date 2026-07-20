f7ea2ce963e2a92cf2faf7bce536ee4f6c5e3dc5
Mario Kahlhofer <mario.kahlhofer@dynatrace.com>
2026-07-20 09:57:51 +0200

    fix: Make patch-crds.sh work on GNU (Linux) and BSD (macOS)

--- hack/make/patch-crds.sh
+++ hack/make/patch-crds.sh
@@ -12,16 +12,24 @@ if [ -d "$CRD_DIR" ]; then
   for file in "$CRD_DIR"/*.yaml; do
     echo "Patching $file with Helm templates ..."
 
-    # Remove document separator if present
-    sed -i '1{/^---$/d}' "$file"
+    awk '
+      # Remove document separator if present
+      NR == 1 && $0 == "---" { next }
 
-    # Prepend 'crd.enable' check
-    sed -i '1i{{- if .Values.crd.enable }}' "$file"
+      # Prepend crd.enable check
+      !guard { print "{{- if .Values.crd.enable }}"; guard = 1 }
 
-    # Append end statement
-    echo "{{- end }}" >> "$file"
+      { print }
 
-    # Insert 'crd.keep' check and annotation
-    sed -i '/controller-gen.kubebuilder.io\/version:/a \    {{- if and .Values.crd.keep .Values.template.helmLabels }}\n    helm.sh/resource-policy: keep\n    {{- end }}' "$file"
+      # Insert crd.keep check and annotation after controller-gen version annotation
+      /controller-gen\.kubebuilder\.io\/version:/ {
+        print "    {{- if and .Values.crd.keep .Values.template.helmLabels }}"
+        print "    helm.sh/resource-policy: keep"
+        print "    {{- end }}"
+      }
+
+      # Append end statement
+      END { print "{{- end }}" }
+    ' "$file" > "$file.tmp" && mv "$file.tmp" "$file"
   done
 fi
